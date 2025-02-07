dfa093b2f5e898176c1d0c8bfe6357a11274c980
Salman <salmaniqbal00786@gmail.com>
2025-02-07 01:39:47 -0600

    adding more projects

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Nav from "./components/Nav.js";
-import Headers from './components/Headers.js';
+import Header from './components/Headers.js';
 import Main from './components/Main.js';
 import Menu from './components/Menu.js';
 import Footer from './components/Footer.js';
@@ -13,7 +13,7 @@ function App() {
  
     <>
     <Nav/>
-    <Headers/>
+    <Header/>
     <Main/>
     <Menu/>
     <MainDetails/>
